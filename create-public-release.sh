34acf6ff4c29cb931c5efb86535b0c9490aa5d3a
Simon Levesque <simon@foilen.com>
2018-06-28 07:41:39 -0400

    Add build scripts

--- create-public-release.sh
+++ create-public-release.sh
@@ -20,42 +20,13 @@ export VERSION=$1
 RUN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $RUN_PATH
 
-echo ----[ Update copyrights ]----
-./scripts/javaheaderchanger.sh > /dev/null
+./step-update-copyrights.sh
+./step-clean-compile.sh
+./step-create-docker-image.sh
+./step-upload-docker-image.sh
+./step-git-tag.sh
 
-echo ----[ Compile ]----
-./gradlew build
-
-echo ----[ Prepare folder for docker image ]----
-DOCKER_BUILD=$RUN_PATH/build/docker
-
-rm -rf $DOCKER_BUILD
-mkdir -p $DOCKER_BUILD/app
-
-cp -v build/libs/foilen-login-$VERSION.jar $DOCKER_BUILD/app/foilen-login.jar
-cp -v docker-release/* $DOCKER_BUILD
-echo -n $VERSION > $DOCKER_BUILD/app/version.txt
-
-echo ----[ Docker image folder content ]----
-find $DOCKER_BUILD
-
-echo ----[ Build docker image ]----
-DOCKER_IMAGE=foilen-login:$VERSION
-docker build -t $DOCKER_IMAGE $DOCKER_BUILD
-
-rm -rf $DOCKER_BUILD 
-
-echo ----[ Upload docker image ]----
-docker login
-docker tag $DOCKER_IMAGE foilen/$DOCKER_IMAGE
-docker tag $DOCKER_IMAGE foilen/foilen-login:latest
-docker push foilen/$DOCKER_IMAGE
-docker push foilen/foilen-login:latest
-
-echo ----[ Git Tag ]==----
-git tag -a -m $VERSION $VERSION
-
-echo ----[ Operation completed successfully ]==----
+echo ----[ Operation completed successfully ]----
 
 echo
 echo You can see published items on https://hub.docker.com/r/foilen/foilen-login/tags/
---
+++ step-create-docker-image.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+RUN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $RUN_PATH
+
+echo ----[ Prepare folder for docker image ]----
+DOCKER_BUILD=$RUN_PATH/build/docker
+
+rm -rf $DOCKER_BUILD
+mkdir -p $DOCKER_BUILD/app
+
+cp -v build/libs/foilen-login-$VERSION.jar $DOCKER_BUILD/app/foilen-login.jar
+cp -v docker-release/* $DOCKER_BUILD
+echo -n $VERSION > $DOCKER_BUILD/app/version.txt
+
+echo ----[ Docker image folder content ]----
+find $DOCKER_BUILD
+
+echo ----[ Build docker image ]----
+DOCKER_IMAGE=foilen-login:$VERSION
+docker build -t $DOCKER_IMAGE $DOCKER_BUILD
+
+rm -rf $DOCKER_BUILD
